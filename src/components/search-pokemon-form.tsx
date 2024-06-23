6674acb0feb015408e619229ab01cf8df92e6c12
Antonov-S <anto1nov1@gmail.com>
2024-06-23 15:31:51 +0300

    Making Search form fully functional and adding Clear button and functionality

--- src/components/search-pokemon-form.tsx
+++ src/components/search-pokemon-form.tsx
@@ -1,50 +1,72 @@
 "use client";
 
-import { FormEvent, useEffect, useRef, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState, ChangeEvent, FormEvent } from "react";
 
 import { Button } from "./ui/button";
 
 function SearchPokemonForm() {
-  const [searchText, setSearchText] = useState("");
-  const inputRef = useRef<HTMLInputElement>(null);
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
-  useEffect(() => {
-    if (inputRef.current) {
-      inputRef.current.focus();
+  const [searchTerm, setSearchTerm] = useState(searchParams.get("query") || "");
+
+  const handleSearch = (term: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
+    if (term) {
+      params.set("query", term);
+      params.delete("type");
+    } else {
+      params.delete("query");
     }
-  }, []);
+    replace(`${pathname}?${params.toString()}`);
+  };
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+    handleSearch(searchTerm);
+  };
 
-    if (!searchText) {
-      if (!inputRef.current) {
-        return;
-      }
+  const handleClear = () => {
+    setSearchTerm("");
+    const params = new URLSearchParams(searchParams);
+    params.delete("query");
+    params.delete("page");
+    replace(`${pathname}?${params.toString()}`);
+  };
 
-      alert("Search text input can't be empty");
-      inputRef.current.focus();
-      return;
-    }
-
-    // onAddItem(itemText);
-    setSearchText("");
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value.toLowerCase());
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="relative">
       <h2 className="text-brown2 text-base font-medium">Search Pokemon</h2>
-      <input
-        ref={inputRef}
-        value={searchText}
-        onChange={e => {
-          setSearchText(e.target.value);
-        }}
-        type="text"
-        autoFocus
-        className="h-[45px] w-full border border-[rgba(0,0,0,0.12)] rounded-[5px] my-[12px] mb-[8px] py-0 px-[16px] text-[14px] italic placeholder:italic"
-      />
-      <Button className="h-[45px] w-full border-none rounded bg-brown3 text-white text-base cursor-pointer flex justify-center items-center transition-all duration-200 hover:bg-brown4">
+      <div className="relative">
+        <input
+          value={searchTerm}
+          onChange={handleChange}
+          type="text"
+          placeholder="Search by name..."
+          autoFocus
+          className="h-[45px] w-full border border-[rgba(0,0,0,0.12)] rounded-[5px] my-[12px] mb-[8px] py-0 px-[16px] text-[14px] italic placeholder:italic"
+        />
+        {searchTerm && (
+          <button
+            type="button"
+            onClick={handleClear}
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-red-500 text-2xl font-bold"
+          >
+            &times;
+          </button>
+        )}
+      </div>
+      <Button
+        type="submit"
+        className="h-[45px] w-full border-none rounded bg-brown3 text-white text-base cursor-pointer flex justify-center items-center transition-all duration-200 hover:bg-brown4"
+      >
         Search
       </Button>
     </form>
